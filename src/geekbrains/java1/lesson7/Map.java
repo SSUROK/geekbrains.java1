248b75f32fa689bd27b0a9da18c66ebf8ef2fb68
Ilay Vinogradov <ilay125114@gmail.com>
2020-08-18 02:13:38 +0300

    commit

--- src/geekbrains/java1/lesson7/Map.java
+++ src/geekbrains/java1/lesson7/Map.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Map extends JPanel {
     public static final int MODE_HVH = 0;
@@ -12,30 +14,35 @@ public class Map extends JPanel {
     private static int currentSizeY = 0;
     private static final int WIN_WIDTH = 507;
     private static final int WIN_HEIGHT = 555;
-    private JButton[] b = new JButton[currentSizeY*currentSizeX];
+    private JButton[][] b = new JButton[currentSizeX][currentSizeY];
     private static final Border border = new LineBorder(Color.BLACK, 2);
+    public static final JPanel panelCenter = new JPanel();
 
     Map() {
         setBackground(Color.DARK_GRAY);
+        centerMaker();
+    }
+
+    private void centerMaker() {
         if (currentSizeX > 0){
-            JPanel panelCenter = new JPanel();
             panelCenter.setLayout(new GridLayout(currentSizeX, currentSizeY));
-            for (int i = 0; i < currentSizeX*currentSizeY; i++) {
-                b[i] = new JButton();
-                b[i].setPreferredSize(new Dimension(WIN_WIDTH / currentSizeX - 5, WIN_HEIGHT / currentSizeY - 20));
-                b[i].setBackground(Color.GRAY);
-                b[i].setForeground(Color.BLACK);
-                b[i].setBorder(border);
-                panelCenter.add(b[i]);
+            for (int i = 0; i < currentSizeX; i++) {
+                for (int y = 0; y < currentSizeY; y++) {
+                    b[i][y] = new JButton("Place Here");
+                    b[i][y].setPreferredSize(new Dimension(WIN_WIDTH / currentSizeX - 5, WIN_HEIGHT / currentSizeY - 20));
+                    b[i][y].setBackground(Color.GRAY);
+                    b[i][y].setForeground(Color.BLACK);
+                    b[i][y].setBorder(border);
+                    panelCenter.add(b[i][y]);
+                }
             }
-            add(panelCenter);
-            ButtonChecker.ButtonChanger(panelCenter,b);
+            add(panelCenter,BorderLayout.CENTER);
+            ButtonChecker.ButtonChanger(b, panelCenter);
         }
     }
 
-
     void startNewGame(int mode, int fieldSizeX, int fieldSizeY, int winLength) {
-        System.out.printf("mode: %d, size: %d, len: %d\n", mode, fieldSizeX, winLength);
+//        System.out.printf("mode: %d, size: %d, len: %d\n", mode, fieldSizeX, winLength);
         currentSizeX = fieldSizeX;
         currentSizeY = fieldSizeY;
         new GameWindow();
