62614950af6ed26ceb3f563247418548e768d71f
Ilay Vinogradov <ilay125114@gmail.com>
2020-09-05 22:22:37 +0300

    v1

---
+++ src/geekbrains/java2/lesson4/ClientGUI.java
@@ -0,0 +1,144 @@
+package geekbrains.java2.lesson4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ClientGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler, KeyListener {
+
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+
+    private final JTextArea log = new JTextArea();
+
+    private final JPanel panelTop = new JPanel(new GridLayout(2, 3));
+    private final JTextField tfIPAddress = new JTextField("127.0.0.1");
+    private final JTextField tfPort = new JTextField("8189");
+    private final JCheckBox cbAlwaysOnTop = new JCheckBox("Always on top");
+    private final JTextField tfLogin = new JTextField("ivan");
+    private final JPasswordField tfPassword = new JPasswordField("123");
+    private final JButton btnLogin = new JButton("Login");
+
+    private final JPanel panelBottom = new JPanel(new BorderLayout());
+    private final JButton btnDisconnect = new JButton("<html><b>Disconnect</b></html>");
+    private final JTextField tfMessage = new JTextField();
+    private final JButton btnSend = new JButton("Send");
+
+    private final JList<String> userList = new JList<>();
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() { // Event Dispatching Thread
+                new ClientGUI();
+            }
+        });
+    }
+
+    private ClientGUI() {
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(WIDTH, HEIGHT);
+        log.setEditable(false);
+        JScrollPane scrollUser = new JScrollPane(userList);
+        JScrollPane scrollLog = new JScrollPane(log);
+        String[] users = {"user1", "user2", "user3", "user4", "user5",
+                "user_with_an_exceptionally_long_name_in_this_chat"};
+        userList.setListData(users);
+        scrollUser.setPreferredSize(new Dimension(100, 0));
+        cbAlwaysOnTop.addActionListener(this);
+        btnSend.addActionListener(this);
+        tfMessage.addKeyListener(this);
+
+        panelTop.add(tfIPAddress);
+        panelTop.add(tfPort);
+        panelTop.add(cbAlwaysOnTop);
+        panelTop.add(tfLogin);
+        panelTop.add(tfPassword);
+        panelTop.add(btnLogin);
+        panelBottom.add(btnDisconnect, BorderLayout.WEST);
+        panelBottom.add(tfMessage, BorderLayout.CENTER);
+        panelBottom.add(btnSend, BorderLayout.EAST);
+
+        add(scrollLog, BorderLayout.CENTER);
+        add(scrollUser, BorderLayout.EAST);
+        add(panelTop, BorderLayout.NORTH);
+        add(panelBottom, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+
+    public void send(){
+        log.append("User: " + tfMessage.getText() + "\n");
+        logCreate("User: " + tfMessage.getText() + "\n");
+        tfMessage.setText("");
+    }
+
+    public void logCreate(String s){
+        try {
+            FileOutputStream fos = new FileOutputStream("LOG.TXT", true);
+
+            fos.write(s.getBytes());
+
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src == cbAlwaysOnTop) {
+            setAlwaysOnTop(cbAlwaysOnTop.isSelected());
+        } else if (src == btnSend){
+            send();
+        } else {
+            throw new RuntimeException("Unknown source: " + src);
+        }
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace();
+        String msg;
+        StackTraceElement[] ste = e.getStackTrace();
+        msg = "Exception in " + t.getName() + " " +
+                e.getClass().getCanonicalName() + ": " +
+                e.getMessage() + "\n\t at " + ste[0];
+        JOptionPane.showMessageDialog(this, msg, "Exception", JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int src = e.getKeyCode();
+        if (src == KeyEvent.VK_ENTER && !tfMessage.getText().equals("")){
+            send();
+        }else{
+//            throw new RuntimeException("Unknown source: " + src);
+        }
+    }
+}
